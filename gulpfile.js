b4dac59324d1f08e9582105c0d889451d3119daf
Leon van der Noll <leon30@live.nl>
2018-05-02 09:43:27 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let connect = require('gulp-connect-php7');
+let browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+	return gulp.src('sass/index.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(gulp.dest('css'))
+		.pipe(browserSync.reload({
+			stream: true
+		}))
+});
+
+gulp.task('browserSync', function() {
+	connect.server({}, function() {
+		browserSync.init({
+			proxy: '127.0.0.1:8000',
+			livereload: true,
+		});
+	});
+});
+
+gulp.task('watch', ['sass', 'browserSync'], function() {
+	gulp.watch('sass/*.scss', ['sass']);
+	gulp.watch('js/*.js', browserSync.reload);
+	gulp.watch('index.html', browserSync.reload);
+});
+
+gulp.task('default', ['watch'], function() {
+});
